fe159645fdf7f176a9cee37e8caefd9d58831cac
Josh Brown <brownjoshalan@gmail.com>
2021-05-19 13:55:41 -0600

    Added get requests to server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //Since this is true; we can post nested objects and parse with qs library
 app.use(express.urlencoded({ extended: true }));
@@ -12,9 +14,26 @@ app.use(express.json());
 //Allows us access to the public folder
 app.use(express.static("public"));
 
-//Routes
-require("./routes");
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+);
 
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
+);
+
+app.get("api/notes", (req, res) =>
+  fs.readFile(
+    path.join(__dirname, "./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        res.sendFile(parsedData);
+      }
+    })
+  )
+);
 //Listen on PORT 8080
 app.listen(PORT, function (err) {
   if (err) console.log(err);
