dc2dbc27d7561b217d7cbb09874449f8a0cc1413
Josh Brown <brownjoshalan@gmail.com>
2021-05-19 09:31:52 -0600

    Added express.static to access other folders that are needed

--- server.js
+++ server.js
@@ -3,8 +3,19 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+//Since this is true; we can post nested objects and parse with qs library
 app.use(express.urlencoded({ extended: true }));
 
+//parse the application into JSON
+app.use(express.json());
+
+//Allows us access to the public folder
+app.use(express.static("public"));
+
+//Routes
+require("./routes");
+
+//Listen on PORT 8080
 app.listen(PORT, function (err) {
   if (err) console.log(err);
   console.log("Server listening on PORT", PORT);
